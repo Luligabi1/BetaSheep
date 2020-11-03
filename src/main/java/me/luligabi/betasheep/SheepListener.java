f19d8145d27bef83903d367ba5bee467f2bd92a2
Luligabi1 <luligabi111@gmail.com>
2020-11-03 15:42:50 -0300

    Disables vanilla shearing

--- src/main/java/me/luligabi/betasheep/SheepListener.java
+++ src/main/java/me/luligabi/betasheep/SheepListener.java
@@ -7,21 +7,26 @@ import org.bukkit.entity.Sheep;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerShearEntityEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class SheepListener implements Listener {
 	
 	@EventHandler
-	public void on(EntityDamageByEntityEvent e) {
+	public void onBetaShearing(EntityDamageByEntityEvent e) { //TODO: Add config to toggle healing the sheep after the hit
 		if(e.getEntityType() != EntityType.SHEEP) return;
 		Sheep s = (Sheep) e.getEntity();
 		if(!s.isAdult()) return;
-		giveWool(s, s.getColor());
+		dropWool(s, s.getColor());
 	}
-	private void giveWool(Sheep s, DyeColor d) {
+	@EventHandler
+	public void onVanillaShearing(PlayerShearEntityEvent e) {
+		e.setCancelled(true); //TODO: Add config to toggle vanilla shearing.
+	}
+	private void dropWool(Sheep s, DyeColor d) {
 		double randomWool = Math.floor(Math.random() * (3 - 1 + 1) + 1); // randomized 1-3 wool drop
 		ItemStack wool = new ItemStack(Material.WOOL, (int) randomWool, d.getData()); 
 		s.setSheared(true);
 		s.getWorld().dropItemNaturally(s.getLocation(), wool); 
 	}
-} //TODO: Disable shearing with shears.
+} 
